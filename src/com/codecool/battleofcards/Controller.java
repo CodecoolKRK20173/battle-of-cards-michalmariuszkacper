de52852801f8454abb0617b01c696a35a563575e
Mariusz Piątkowski <mz.piatkowski@gmail.com>
2019-04-16 14:21:35 +0200

    View, Controller

--- src/com/codecool/battleofcards/Controller.java
+++ src/com/codecool/battleofcards/Controller.java
@@ -1,5 +1,55 @@
 package com.codecool.battleofcards;
 
+
 class Controller {
+    private View view = new View();
+    private Game game = new Game();
+    
+    
+    public void runGame(){
+        String selectionOfMenu = "";
+        do{
+            view.displayMenu();
+            view.displayMessage(view.validateSelectionOfMenu(selectionOfMenu));
+
+            selectionOfMenu = view.getInput("Selection: ");
+            switch (selectionOfMenu) {
+                case "1":
+                    startGame();
+                    break;
+                case "2":
+                    System.out.println("You picked option 2");
+                    break;
+                case "3":
+                    System.out.println("Closed correctly.");
+                    break;
+            }
+        }while(view.validateSelectionOfMenu(selectionOfMenu));
+        
+    }
+
+    public void startGame() {
+        String typeOfGame = "";
+        do{
+            view.displayTypeOfGame();
+            view.displayMessage(view.validateTypeOfGameSelection(typeOfGame));
+            
+            typeOfGame = view.getInput("Type of game: ");
+            switch (typeOfGame) {
+                case "1":
+
+                    game.createPlayers();
+                    break;
+                case "2":
+                    System.out.println("You picked option 2");
+                    break;
+                case "3":
+                    runGame();
+                    break;
+            }
+        }while(view.validateTypeOfGameSelection(typeOfGame));
+        
+    }
+
     
 }
--- src/com/codecool/battleofcards/Main.java
+++ src/com/codecool/battleofcards/Main.java
@@ -1,8 +1,10 @@
 package com.codecool.battleofcards;
 
-class Main {
+public class Main {
+
 
     public static void main(String[] args) {
-        
+        Controller controller = new Controller();
+        controller.runGame();
     }
 }
